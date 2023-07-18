9cf795dcf3a405a61d61d54b161d0e03fec148f1
RohanRainkwar <119723004+RohanRaikwar@users.noreply.github.com>
2023-07-19 00:49:54 +0530

    Delete src directory

--- src/service/opt.genrater.ts
+++
@@ -1,7 +0,0 @@
-const otpGenerator = require('otp-generator')
-const otpgen = async()=>{
-      const otp  =  await otpGenerator.generate(4, {digits:true, upperCaseAlphabets: false, lowerCaseAlphabets:false,specialChars: false });
-      return otp
-
-}
-export default otpgen
