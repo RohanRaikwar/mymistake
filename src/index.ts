de69ccb9116bc0717081464973c15d4c972c4e2f
RohanRaikwar <rohansingh9675@gmail.com>
2023-08-04 10:59:41 +0530

    romms requied update

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ connectDB();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-app.use(express.static(path.join('public/')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
